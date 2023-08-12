031ea17f600c10545f56929e88f4e3d5f368f0c2
Chuya Goto <pillochu5@gmail.com>
2023-08-12 19:57:09 +0900

    nodebrew

--- cookbooks/nodejs/default.rb
+++ cookbooks/nodejs/default.rb
@@ -1,1 +1,10 @@
 package 'nodebrew'
+
+execute 'install latest ruby' do
+    not_if "ls $HOME/.nodebrew/current/bin/nodebrew"
+    
+    command <<~SCRIPT
+    nodebrew setup
+    echo 'export PATH=$HOME/.nodebrew/current/bin:$PATH' >> ~/.zprofile
+    SCRIPT
+end
