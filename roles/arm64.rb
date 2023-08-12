7a9370c88343f68966bb687cd758e331095bd96a
Chuya Goto <pillochu5@gmail.com>
2023-08-12 20:12:49 +0900

    fix

--- roles/arm64.rb
+++ roles/arm64.rb
@@ -4,3 +4,16 @@ include_recipe "../cookbooks/cask"
 include_recipe "../cookbooks/ruby"
 include_recipe "../cookbooks/nodejs"
 include_recipe "../cookbooks/vscode"
+
+package "mysql@5.7"
+command "brew services start mysql@5.7"
+command 'which mysql || echo export PATH="/usr/local/opt/mysql@5.7/bin:$PATH" >> ~/.zprofile'
+
+package "redis"
+command "brew services start redis"
+
+package "td-agent"
+command "launchctl load /Library/LaunchDaemons/td-agent.plist"
+
+package "imagemagick@6"
+package "ghostscript"
