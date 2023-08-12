69ecb38a9b480fdea0505577a46a56e3bf8610bc
Chuya Goto <pillochu5@gmail.com>
2023-08-12 20:04:10 +0900

    fix

--- roles/arm64.rb
+++ roles/arm64.rb
@@ -1,4 +1,6 @@
 node.reverse_merge!(user: ENV['SUDO_USER'] || ENV['USER'])
 
+include_recipe "../cookbooks/cask"
 include_recipe "../cookbooks/ruby"
 include_recipe "../cookbooks/nodejs"
+include_recipe "../cookbooks/vscode"
